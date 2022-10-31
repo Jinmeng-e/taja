b58c590bd8225d9bc6820fd81ca7ca5ea48dddba
Jinmeng-e <32565199+Jinmeng-e@users.noreply.github.com>
2022-11-01 04:27:49 +0900

    test version

---
+++ Taja/Assets/02.Dev/Main.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Transcribe
+{
+    public class Main : MonoBehaviour
+    {
+        public static Main Inst => inst;
+        static Main inst;
+
+
+        [SerializeField] Image innerBG;
+        [SerializeField] Image BG;
+
+        private void Awake()
+        {
+            inst = this;
+        }
+
+        public void OnInnerColorChanged(Color color)
+        {
+            innerBG.color = color ;
+        }
+        public void OnBGChanged(Sprite sprite)
+        {
+            BG.sprite = sprite;
+        }
+        //Get UserData
+        //Get BookData
+        //ShowMain
+        //
+    }
+}
