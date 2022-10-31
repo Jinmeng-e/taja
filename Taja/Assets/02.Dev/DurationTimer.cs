cde9ca3dc1071b9ca8f9bc3de4c254c97a13ed82
Jinmeng-e <32565199+Jinmeng-e@users.noreply.github.com>
2022-11-01 06:23:56 +0900

    빌드 테스트 완료(어색함)

---
+++ Taja/Assets/02.Dev/DurationTimer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DurationTimer : MonoBehaviour
+{
+
+
+    private float durationValue;
+    private TimeSpan duration;
+}
--- Taja/Assets/02.Dev/UI.cs
+++ Taja/Assets/02.Dev/UI.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static UI Inst;
+    static UI inst;
+
+    public void Awake()
     {
-        
+        inst = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowUI()
     {
-        
     }
 }
